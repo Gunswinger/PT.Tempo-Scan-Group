b33ba8c99f8a9e1c07571ed9dd9391bd92688ad0
Gunswinger <gunswinger0@gmail.com>
2019-11-06 23:34:10 +0700

    Aminte.css adding

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var rellax = new Rellax(".rellax", {
+  speed: -2,
+  center: true,
+  wrapper: null,
+  round: true,
+  vertical: true,
+  horizontal: false
+});
+
+/* Rellax.js is a parallax library that uses vanilla Javascript */
