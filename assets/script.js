7e27f764b363f38b87df084a7365d767c5ff6af0
Gunswinger <gunswinger0@gmail.com>
2019-11-07 00:18:12 +0700

    Animate Image

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,19 @@
 var rellax = new Rellax(".rellax", {
-  speed: -2,
-  center: true,
-  wrapper: null,
-  round: true,
-  vertical: true,
-  horizontal: false
+    speed: -2,
+    center: true,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
 });
 
 /* Rellax.js is a parallax library that uses vanilla Javascript */
+
+$(window).scroll(function() {
+    var tScroll = $(this).scrollTop();
+    if (tScroll > $('.col-sm').offset().top) {
+        $('.col-sm img').each(function() {
+            $('.col-sm img').addClass('animated zoomIn');
+        });
+    }
+});
